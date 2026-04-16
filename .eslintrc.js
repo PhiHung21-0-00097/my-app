20137faaaf960beb71bc31e5ebf874b3677b9d36
Otis <0943244904hung@gmail.com>
2026-04-16 17:13:37 +0700

    up

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ... các cấu hình khác
+  rules: {
+    // Tắt kiểm tra biến đã định nghĩa nhưng không sử dụng
+    "@typescript-eslint/no-unused-vars": "off",
+
+    // Tắt kiểm tra việc sử dụng kiểu 'any'
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
